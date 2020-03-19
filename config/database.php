4f049acd9bbfd7fd006508220f7206fc90a708ba
Noufeine <ahmednoufeine@gmail.com>
2020-03-19 19:05:50 +0100

    modification config BD

--- config/database.php
+++ config/database.php
@@ -48,34 +48,25 @@ return [
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env( 'DB_HOST', '127.0.0.1' ),
-            'port'        => env( 'DB_PORT', '3308' ),
+            'port'        => env( 'DB_PORT', '8889' ),
             'database'    => env( 'DB_DATABASE', 'BDPSI' ),
             'username'    => env( 'DB_USERNAME', 'root' ),
-            'password'    => env( 'DB_PASSWORD', '' ),
+            'password'    => env( 'DB_PASSWORD', 'root' ),
             'unix_socket' => env( 'DB_SOCKET', '' ),
             'charset'     => 'utf8mb4',
             'collation'   => 'utf8mb4_unicode_ci',
             'prefix'      => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-        ]) : [],
-    ],
-
-
-            /*'strict'      => true,
+            'strict'      => true,
             'engine'      => null,
             'modes'       => [
-                /*'ONLY_FULL_GROUP_BY',
+                'ONLY_FULL_GROUP_BY',
                 'STRICT_TRANS_TABLES',
                 'NO_ZERO_IN_DATE',
                 'NO_ZERO_DATE',
                 'ERROR_FOR_DIVISION_BY_ZERO',
                 'NO_ENGINE_SUBSTITUTION',
             ],
-        ],*/
+        ],
 
 
         'pgsql' => [
