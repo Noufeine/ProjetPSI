400e6e4cee2850eab2b7310cb7baa1959d57cb28
Chelson Supreme <chelson.sm@gmail.com>
2020-03-19 18:21:36 +0100

    ajout page acceuil

--- config/database.php
+++ config/database.php
@@ -48,25 +48,34 @@ return [
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env( 'DB_HOST', '127.0.0.1' ),
-            'port'        => env( 'DB_PORT', '8889' ),
-            'database'    => env( 'DB_DATABASE', 'forge' ),
-            'username'    => env( 'DB_USERNAME', 'forge' ),
+            'port'        => env( 'DB_PORT', '3308' ),
+            'database'    => env( 'DB_DATABASE', 'BDPSI' ),
+            'username'    => env( 'DB_USERNAME', 'root' ),
             'password'    => env( 'DB_PASSWORD', '' ),
             'unix_socket' => env( 'DB_SOCKET', '' ),
             'charset'     => 'utf8mb4',
             'collation'   => 'utf8mb4_unicode_ci',
             'prefix'      => '',
-            'strict'      => true,
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        ]) : [],
+    ],
+
+
+            /*'strict'      => true,
             'engine'      => null,
             'modes'       => [
-                'ONLY_FULL_GROUP_BY',
+                /*'ONLY_FULL_GROUP_BY',
                 'STRICT_TRANS_TABLES',
                 'NO_ZERO_IN_DATE',
                 'NO_ZERO_DATE',
                 'ERROR_FOR_DIVISION_BY_ZERO',
                 'NO_ENGINE_SUBSTITUTION',
             ],
-        ],
+        ],*/
 
 
         'pgsql' => [
