3d938ad707702e11d8957e04b0654f254a380931
Chelson Supreme <chelson.sm@gmail.com>
2020-03-23 21:13:43 +0100

    ajout et liste

--- config/database.php
+++ config/database.php
@@ -48,8 +48,8 @@ return [
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env( 'DB_HOST', '127.0.0.1' ),
-            'port'        => env( 'DB_PORT', '3308' ),
-            'database'    => env( 'DB_DATABASE', 'BDPSI' ),
+            'port'        => env( 'DB_PORT', '3301' ),
+            'database'    => env( 'DB_DATABASE', 'bdpsi' ),
             'username'    => env( 'DB_USERNAME', 'root' ),
             'password'    => env( 'DB_PASSWORD', '' ),
             'unix_socket' => env( 'DB_SOCKET', '' ),
