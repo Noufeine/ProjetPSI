495ae515e32d40df82585134cda7a1462ae4ddfa
Aniss Elarj <aniss.elarj92@gmail.com>
2020-03-30 23:22:15 +0200

    import/export individus

--- config/database.php
+++ config/database.php
@@ -48,10 +48,10 @@ return [
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env( 'DB_HOST', '127.0.0.1' ),
-            'port'        => env( 'DB_PORT', '8889' ),
+            'port'        => env( 'DB_PORT', '3306' ),
             'database'    => env( 'DB_DATABASE', 'BDPSI' ),
             'username'    => env( 'DB_USERNAME', 'root' ),
-            'password'    => env( 'DB_PASSWORD', 'root' ),
+            'password'    => env( 'DB_PASSWORD', '' ),
             'unix_socket' => env( 'DB_SOCKET', '' ),
             'charset'     => 'utf8mb4',
             'collation'   => 'utf8mb4_unicode_ci',
